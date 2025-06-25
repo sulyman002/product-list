191616b3d54d3608b3be13edf2d3e36b1adaf958
sulyman002 <oyedelesulaiman@gmail.com>
2025-06-25 19:44:11 +0100

    working on it

--- script.js
+++ script.js
@@ -100,45 +100,144 @@ const productListData = [
     }
 ]
 
-const cardsContainer = document.getElementById('cardsContainer');
+const generalContainer = document.getElementById('generalContainer');
 
 productListData.forEach(card => {
-    //container for the card
-    const cardTemplate = document.createElement('div');
-    cardTemplate.className ='cardTemp'
-    cardTemplate.id = 'cardT'
-    //link to the parent
-    cardTemplate.append(imgCart);
-    cardTemplate.append(cardText);
-    // children  first
-    const imgCart = document.createElement('div');
-    imgCart.className = 'imgCartHolder'
-    imgCart.append(img);
-    imgCart.append(cart);
-    // first child too
-    const img = document.createElement('img');
-    img.className = 'imgs';
+    const cardsContainer = document.createElement('div');
 
-    const cart = document.createElement('div');
-    cart.className = 'carts';
-    
+    cardsContainer.innerHTML = `
+       <div
+            class="active:border-2 active:border-blue-900 rounded-lg relative flex w-full items-center justify-center">
+            <img class="rounded-lg" src="${card.image.desktop}" alt="">
 
-    //children second
+            <div
+                class=" hidden h-12 flex absolute items-center active:border-2 shadow active:border-blue-900 justify-center w-2/3 bg-white rounded-full py-3 gap-2">
+                <img src="/assets/images/icon-add-to-cart.svg" alt="">
+                <p class="font-semibold text-sm">Add to Cart</p>
 
-    const cardText = document.createElement('cardText');
-    cardText.className = 'cardTex';
-    cardText.append(p1);
-    cardText.append(h);
-    cardText.append(p2);
-    
-    // second child too
-    const p1 = document.createElement('p');
-    const h = document.createElement('h2');
-    const p2 = document.createElement('p');
+            </div>
 
-    p1.className = 'nam'
-    h.className = 'categ'
-    p2.className = 'pric'
+            <div
+                class="h-12 text-white flex absolute items-center active:border-2 shadow active:border-blue-900 justify-between w-2/3 bg-red-600 rounded-full py-3 px-6 gap-2">
+                <img class="border-[1px] border-white p-2 rounded-full" src="/assets/images/icon-decrement-quantity.svg" alt="">
+                <p>0</p>
+                <img class="border-[1px] border-white p-2 rounded-full" src="/assets/images/icon-increment-quantity.svg" alt="">
+            </div>
 
+        </div>
+                    
+        <div class="mt-6 leading-snug">
+            <p class="text-sm font-lighter text-slate-600">${card.name}</p>
+            <h2 class="font-semibold">${card.category}</h2>
+            <p class="font-semibold text-red-500">$${card.price.toFixed(2)}</p>
+        </div> 
+    `
+    generalContainer.appendChild(cardsContainer)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// productListData.forEach(card => {
+//     //container for the card
+//     const cardTemplate = document.createElement('div');
+//     cardTemplate.className ='cardTemp';
+//     cardTemplate.id = 'cardT';
+//     //link to the parent
+//     cardTemplate.append(imgCart);
+//     cardTemplate.append(cardText);
+//     // children  first
+//     const imgCart = document.createElement('div');
+//     imgCart.className = 'imgCartHolder';
+//     imgCart.append(img);
+//     imgCart.append(cart);
+//     // first child too
+//     const img = document.createElement('img');
+//     img.className = 'imgs';
+
+//     const cart = document.createElement('div');
+//     cart.className = 'carts';
+    
+
+//     //children second
+
+//     const cardText = document.createElement('cardText');
+//     cardText.className = 'cardTex';
+//     cardText.append(p1);
+//     cardText.append(h);
+//     cardText.append(p2);
+    
+//     // second child too
+//     const p1 = document.createElement('p');
+//     const h = document.createElement('h2');
+//     const p2 = document.createElement('p');
+
+//     p1.className = 'nam';
+//     h.className = 'categ';
+//     p2.className = 'pric';
+
+//     // the function now
+
+//     img.textContent = `${card.image.mobile}`;
+//     cart.textContent = `url(.../)`
+
+// });
 
